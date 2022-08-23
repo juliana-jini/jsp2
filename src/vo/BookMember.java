fde2604751ae689ef6aef6c37b6d7434aeffa89c
juliana-jini <julianaaqr02@gmail.com>
2022-08-23 15:56:42 +0900

    jsp2 수업프로젝트

---
+++ src/vo/BookMember.java
@@ -0,0 +1,17 @@
+package vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor		//vo 클래스 모든 프로퍼티를 select 하지 않는다면 필요합니다.
+@ToString
+public class BookMember {
+	private String name;
+	private String email;
+	private String tel;
+	private String password;
+}	
